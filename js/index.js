f9231e8c08f3a3fa32309a0a265b622d73d81df9
Max <65284135+ma32kc@users.noreply.github.com>
2022-03-05 14:13:51 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -399,4 +399,28 @@ document.addEventListener('DOMContentLoaded', () => {
          }
       }
    }
+
+   var elem = document.documentElement;
+
+   function openFullscreen() {
+      if (elem.requestFullscreen) {
+         elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) { /* Safari */
+         elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE11 */
+         elem.msRequestFullscreen();
+      }
+   }
+
+   function closeFullscreen() {
+      if (document.exitFullscreen) {
+         document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) { /* Safari */
+         document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) { /* IE11 */
+         document.msExitFullscreen();
+      }
+   }
+   document.querySelector('.header__btn__open').addEventListener('click', openFullscreen)
+   document.querySelector('.header__btn__close').addEventListener('click', closeFullscreen)
 })
